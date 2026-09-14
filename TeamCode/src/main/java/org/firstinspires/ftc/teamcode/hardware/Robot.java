ecbf999ddc81b69b0a0467fe364dbd334daac40c
PepeneleZ <pepegame96@gmail.com>
2026-09-14 21:10:55 +0300

    Lynx manager testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -7,12 +7,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.lib.interfaces.Updateable;
 
 public class Robot implements Updateable {
+    private final LynxManager lynxManager;
 
     public Robot(@NonNull HardwareMap hardwareMap) {
+        this.lynxManager = new LynxManager(hardwareMap);
     }
 
     @Override
     public void update() {
-
+        // Linia asta trebuie MEREU pastrata prima in update, NU O STERGETI!!!
+        lynxManager.update();
     }
 }
